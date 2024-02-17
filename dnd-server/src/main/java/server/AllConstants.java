ed9501f589f3dc8f64df2856e407e93339f61068
4Siris <leonid_voynov@mail.ru>
2024-02-17 16:34:42 +0300

    feat: add labyrinth generation logic

--- dnd-server/src/main/java/server/AllConstants.java
+++ dnd-server/src/main/java/server/AllConstants.java
@@ -3,9 +3,10 @@ package server;
 public enum AllConstants {;
 
     public enum IntegerConstants {
-        MAX_MAP_WIDTH(9),
-        MAX_MAP_HEIGHT(8),
-        MAX_ROOM_LEVEL(5);
+        MAX_MAP_WIDTH(7),
+        MAX_MAP_HEIGHT(7),
+        MAX_ROOM_LEVEL(5),
+        TUNNEL_DIVIDING_CHANCE(65);
 
         private int value;
         IntegerConstants(int i) {
