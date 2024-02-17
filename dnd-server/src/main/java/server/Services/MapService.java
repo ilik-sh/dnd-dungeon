e64b1ec44959f6e0d73b056afb35c61d961aa365
4Siris <leonid_voynov@mail.ru>
2024-02-17 16:31:02 +0300

    refactor: change map type

--- dnd-server/src/main/java/server/Services/MapService.java
+++ dnd-server/src/main/java/server/Services/MapService.java
@@ -2,21 +2,17 @@ package server.Services;
 
 import org.springframework.stereotype.Service;
 import server.AllConstants;
-import server.Models.Cell;
+import server.Models.Room;
+import server.RoomDirection;
 
 @Service
 public class MapService {
-    private Cell[][] map;
+    private Room[][] map;
 
     public void generateMap(){
-        map = new Cell[AllConstants.IntegerConstants.MAX_MAP_HEIGHT.getValue()][];
+        map = new Room[AllConstants.IntegerConstants.MAX_MAP_HEIGHT.getValue()][];
         for(int i = 0; i < map.length ; i++){
-            if( i % 2 == 0 ){
-               map[i] = new Cell[AllConstants.IntegerConstants.MAX_MAP_WIDTH.getValue()-1];
-            }
-            if( i % 2 == 1 ){
-                map[i] = new Cell[AllConstants.IntegerConstants.MAX_MAP_WIDTH.getValue()];
-            }
+            map[i] = new Room[AllConstants.IntegerConstants.MAX_MAP_WIDTH.getValue()];
         }
     }
     public void generateMapRooms(Cell[][] map, int phasesAmount){
