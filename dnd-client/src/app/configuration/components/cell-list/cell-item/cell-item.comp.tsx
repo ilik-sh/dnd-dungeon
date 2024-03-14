2470d148e7a71973016bb334884d9307737b5d91
Ilya <i.shyshparonak@gmail.com>
2024-03-14 09:28:41 +0300

    feat: update cell item

--- dnd-client/src/app/configuration/components/cell-list/cell-item/cell-item.comp.tsx
+++ dnd-client/src/app/configuration/components/cell-list/cell-item/cell-item.comp.tsx
@@ -10,9 +10,11 @@ import { TypeColors } from "enums/type-colors.enum";
 import { RoomChildDto } from "app/configuration/types/room-child.dto";
 import { CellDto } from "app/configuration/types/cell.dto";
 import AddRoomButton from "./add-room-button.comp";
-import { addRoom } from "app/configuration/store/manual-generation/manual-generation-config.slice";
+import { addRoom } from "app/map/store/map.slice";
 import { v4 } from "uuid";
 import { RoomType } from "enums/room-type.enum";
+import { defaultDirections } from "app/configuration/default-objects/default-directions";
+import { amber } from "@mui/material/colors";
 
 type CellItemProps = {
   cell: CellDto;
@@ -43,76 +45,68 @@ const CustomGrid = styled(Box)({
   display: "grid",
   gridTemplateColumns: "repeat(auto-fit, minmax(300px, 1fr))",
   gap: "20px",
+  overflowY: "auto",
+  overflowX: "hidden",
+  maxHeight: "80vh",
 });
 
 const SummaryBox = styled("div")({
   alignItems: "center",
   display: "flex",
   gap: "20px",
+  marginBottom: "20px",
 });
 
 function CellItem({ cell }: CellItemProps) {
   const dispatch = useAppDispatch();
 
   const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    const variant: RoomChildDto = {
+    const room: RoomChildDto = {
       id: v4(),
       parentId: cell.id,
       type: "neutral" as RoomType,
       level: 1,
       description: " ",
+      directions: defaultDirections,
+      isVisited: false,
     };
-
-    const addVariantObject = {
-      id: cell.id,
-      variant,
-    };
-    dispatch(addRoom(addVariantObject));
+    dispatch(addRoom({ room }));
   };
   return (
-    <Accordion>
-      <AccordionSummary
-        expandIcon={<ExpandMoreIcon />}
-        aria-controls="panel1-content"
-        id="panel1-header"
-      >
-        <SummaryBox>
-          <StyledStack direction="column">
-            {cell.rooms.map((room, index) => (
-              <RoomRectangle
-                key={index}
-                sx={{
-                  zIndex: `${9 - index}`,
-                  color: TypeColors[room.type].light,
-                  transform: `translateY(${
-                    index * -3
-                  }px) rotateX(60deg) rotateZ(20deg) rotateY(0deg)`,
-                  textShadow: `0px 5px 5px ${
-                    TypeColors[room.type].dark
-                  }, 2px 5px 5px ${TypeColors[room.type].dark}`,
-                }}
-              >
-                &#x2B22;
-              </RoomRectangle>
-            ))}
-          </StyledStack>
-          <Typography>Room #1</Typography>
-        </SummaryBox>
-      </AccordionSummary>
-      <AccordionDetails>
-        <CustomGrid>
+    <>
+      <SummaryBox>
+        <StyledStack direction="column">
           {cell.rooms.map((room, index) => (
-            <Box key={index}>
-              <Room selected={cell.currentRoom?.id == room.id} room={room} />
-            </Box>
+            <RoomRectangle
+              key={index}
+              sx={{
+                zIndex: `${9 - index}`,
+                color: TypeColors[room.type].light,
+                transform: `translateY(${
+                  index * -3
+                }px) rotateX(60deg) rotateZ(20deg) rotateY(0deg)`,
+                textShadow: `0px 5px 5px ${
+                  TypeColors[room.type].dark
+                }, 2px 5px 5px ${TypeColors[room.type].dark}`,
+              }}
+            >
+              &#x2B22;
+            </RoomRectangle>
           ))}
-          <Box>
-            <AddRoomButton handleClick={handleClick} />
+        </StyledStack>
+      </SummaryBox>
+      <CustomGrid>
+        {cell.rooms.map((room, index) => (
+          <Box key={index}>
+            <Room selected={cell.currentRoom?.id == room.id} room={room} />
           </Box>
-        </CustomGrid>
-      </AccordionDetails>
-    </Accordion>
+        ))}
+        <Box>
+          <AddRoomButton handleClick={handleClick} />
+        </Box>
+      </CustomGrid>
+    </>
   );
 }
 
-export default React.memo(CellItem);
+export default CellItem;
