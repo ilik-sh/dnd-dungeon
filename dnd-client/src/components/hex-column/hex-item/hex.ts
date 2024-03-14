f4f735d980d401a4580fd0227fdc7eda4afeb9e5
Ilya <i.shyshparonak@gmail.com>
2024-03-14 09:24:28 +0300

    feat: add dimensions getter and remove rounding from points calculation

--- dnd-client/src/components/hex-column/hex-item/hex.ts
+++ dnd-client/src/components/hex-column/hex-item/hex.ts
@@ -6,26 +6,37 @@ export class Hex {
   constructor(size: number) {
     this.size = size;
   }
+
+  get dimensions() {
+    return {
+      width: this.size * 2,
+      height: this.size * Math.sqrt(3),
+    };
+  }
+
   get center() {
     return {
       x: this.size,
       y: (Math.sqrt(3) / 2) * this.size,
     };
   }
+
   get points() {
     const points = [];
 
     for (let i = 0; i < 6; i++) {
-      const pointX = Math.round(
-        this.center.x + this.size * Math.cos((Math.PI / 180) * 60 * i)
-      );
-      const pointY = Math.round(
-        this.center.y + this.size * Math.sin((Math.PI / 180) * 60 * i)
-      );
+      const angle = 60 * i;
+      const pointX =
+        this.center.x + this.size * Math.cos((Math.PI / 180) * angle);
+
+      const pointY =
+        this.center.y + this.size * Math.sin((Math.PI / 180) * angle);
+
       points.push(`${pointX} ${pointY}`);
     }
     return points;
   }
+
   get sideCenters() {
     const sideCenters = [];
 
@@ -51,27 +62,14 @@ export class Hex {
       TOP_RIGHT: [],
     };
 
-    lines["TOP_LEFT"] = [
-      `${this.center.x} ${this.center.y}`,
-      this.sideCenters[4],
-    ];
-    lines["TOP"] = [`${this.center.x} ${this.center.y}`, this.sideCenters[5]];
-    lines["TOP_RIGHT"] = [
-      `${this.center.x} ${this.center.y}`,
-      this.sideCenters[0],
-    ];
-    lines["BOTTOM_LEFT"] = [
-      `${this.center.x} ${this.center.y}`,
-      this.sideCenters[3],
-    ];
-    lines["BOTTOM"] = [
-      `${this.center.x} ${this.center.y}`,
-      this.sideCenters[2],
-    ];
-    lines["BOTTOM_RIGHT"] = [
-      `${this.center.x} ${this.center.y}`,
-      this.sideCenters[1],
-    ];
+    lines["TOP_LEFT"] = [this.points[3], this.points[4]];
+
+    lines["TOP"] = [this.points[4], this.points[5]];
+
+    lines["TOP_RIGHT"] = [this.points[5], this.points[0]];
+    lines["BOTTOM_LEFT"] = [this.points[2], this.points[3]];
+    lines["BOTTOM"] = [this.points[1], this.points[2]];
+    lines["BOTTOM_RIGHT"] = [this.points[0], this.points[1]];
     return lines;
   }
 }
