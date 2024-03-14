19a537174cff8a2db8b83a515d96f9b2ffcc693c
Ilya <i.shyshparonak@gmail.com>
2024-03-14 09:25:03 +0300

    feat: update room types

--- dnd-client/src/enums/directions.enum.ts
+++ dnd-client/src/enums/directions.enum.ts
@@ -1,8 +1,10 @@
 export enum Directions {
   TOP_LEFT = "TOP_LEFT",
   TOP = "TOP",
-  BOTTOM = "BOTTOM",
+  TOP_RIGHT = "BOTTOM",
   BOTTOM_RIGHT = "BOTTOM_RIGHT",
-  BOTTOM_LEFT = "BOTTOM_LEFT",
-  TOP_RIGHT = "TOP_RIGHT",
+  BOTTOM = "BOTTOM_LEFT",
+  BOTTOM_LEFT = "TOP_RIGHT",
 }
+
+export type Direction = keyof typeof Directions;
