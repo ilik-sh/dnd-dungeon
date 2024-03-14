b8e11e9d7ffbbb780821badee96e27167b04032f
Ilya <i.shyshparonak@gmail.com>
2024-03-14 09:21:19 +0300

    feat: update default objects

--- dnd-client/src/app/configuration/default-objects/default-room-variant.ts
+++
@@ -1,6 +0,0 @@
-import { RoomType } from "enums/room-type.enum";
-import { v4 } from "uuid";
-import { RoomVariantDto } from "../../../types/room.dto";
-import { RoomDto } from "../types/cell.dto";
-
-export const generateDefaultRoomVariant = () => {};
