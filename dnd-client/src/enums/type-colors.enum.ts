5dad01cd936c45097341ff712dfaef3a3b17a88c
Ilya <i.shyshparonak@gmail.com>
2024-03-14 09:32:41 +0300

    feat: update room colors

--- dnd-client/src/enums/type-colors.enum.ts
+++ dnd-client/src/enums/type-colors.enum.ts
@@ -1,8 +1,15 @@
 export const TypeColors = {
-  neutral: { light: "#1e90ff", dark: "#0059b3" },
-  peace: { light: "#006400", dark: "#003300" },
-  evil: { light: "#b22222", dark: "#6b1414" },
-  loot: { light: "#FFD700", dark: "#b39800" },
-  quest: { light: "#9932cc", dark: "#5c1e7b" },
-  absense: { light: "#858383", dark: "#525252" },
+  neutral: { light: "#1e88e5", dark: "#0d47a1" }, // blue
+  peace: { light: "#558b2f", dark: "#3B6434" }, // green
+  evil: { light: "#e57373", dark: "#b71c1c" }, // red
+  loot: { light: "#ffe57f", dark: "#BDB919" }, // yellow/gold
+  quest: { light: "#9575cd", dark: "#5e35b1" }, // purple
+  absense: { light: "#858383", dark: "#525252" }, //grey
 };
+
+// neutral: { light: "#1e90ff", dark: "#0059b3" }, // blue
+//   peace: { light: "#006400", dark: "#003300" }, // green
+//   evil: { light: "#b22222", dark: "#6b1414" }, // red
+//   loot: { light: "#FFD700", dark: "#b39800" }, // yellow/gold
+//   quest: { light: "#9932cc", dark: "#5c1e7b" }, // purple
+//   absense: { light: "#858383", dark: "#525252" }, //grey
