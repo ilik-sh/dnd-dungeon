8bdd5adfbb4c0ac276855a3d1189a560417e4fb3
Ilya <i.shyshparonak@gmail.com>
2024-03-14 09:22:13 +0300

    feat: add ThemeProvider element

--- dnd-client/src/App.tsx
+++ dnd-client/src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "app.routes";
 import { Provider } from "react-redux";
 import store from "store";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "theme/theme";
 
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </ThemeProvider>
     </Provider>
   );
 }
