ad01dbcfc7af7e5aeba524ff32feb415c44a0690
Ilya <i.shyshparonak@gmail.com>
2024-03-17 12:04:28 +0300

    feat: create persistent redux store

--- dnd-client/src/App.tsx
+++ dnd-client/src/App.tsx
@@ -2,18 +2,24 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "app.routes";
 import { Provider } from "react-redux";
-import store from "store";
+import store, { persistor } from "store";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "theme/theme";
+import { SnackbarProvider } from "notistack";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <SnackbarProvider>
+            <BrowserRouter>
+              <AppRoutes />
+            </BrowserRouter>
+          </SnackbarProvider>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- dnd-client/src/store.ts
+++ dnd-client/src/store.ts
@@ -1,15 +1,28 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import manualGenerationConfigSlice from "app/configuration/store/manual-generation/manual-generation-config.slice";
+import storage from "redux-persist/lib/storage";
 import mapSlice from "app/map/store/map.slice";
+import { persistReducer, persistStore } from "redux-persist";
+import { thunk } from "redux-thunk";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const userReducers = combineReducers({
+  manualGenerationConfig: manualGenerationConfigSlice.reducer,
+  map: mapSlice.reducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, userReducers);
 
 const store = configureStore({
-  reducer: {
-    manualGenerationConfig: manualGenerationConfigSlice.reducer,
-    map: mapSlice.reducer,
-  },
+  reducer: persistedReducer,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
+export const persistor = persistStore(store);
 export default store;
