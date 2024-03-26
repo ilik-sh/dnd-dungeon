db9d1b29ebbe1702abc8292402fb3682ec89fba4
Ilya <i.shyshparonak@gmail.com>
2024-03-26 16:18:19 +0300

    feat: drastically remake header component

--- dnd-client/src/app/configuration/components/header/header.comp.tsx
+++ dnd-client/src/app/configuration/components/header/header.comp.tsx
@@ -1,29 +1,136 @@
-import { Container, Typography, styled } from "@mui/material";
-import { amber, blueGrey, green, grey } from "@mui/material/colors";
-import React from "react";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { DungeonDoor } from "assets/icons/dungeon-door.icon";
+import { CloseOutlined, Hexagon } from "@mui/icons-material";
+import { useAppDispatch } from "hooks/redux.hooks";
+import { openModal } from "store/modals.slice";
 
-type Props = {};
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
 
-const StyledHeader = styled(Container)(({ theme }) => ({
-  backgroundColor: grey[800],
-  borderBottomLeftRadius: "10px",
-  borderBottomRightRadius: "10px",
-  padding: "20px",
-  justifyContent: "center",
-  alignItems: "center",
-  display: "flex",
-}));
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
 
-const StyledTypography = styled(Typography)(({ theme }) => ({
-  fontWeight: "900",
+export default function Header(props: Props) {
+  const { window } = props;
+  const dispatch = useAppDispatch();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  color: theme.palette.primary.main,
-}));
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
 
-export default function Header({}: Props) {
+  const handleSignInClicked = () => {
+    dispatch(openModal("signIn"));
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
-    <StyledHeader>
-      <StyledTypography variant="h4">DnD Dungeon</StyledTypography>
-    </StyledHeader>
+    <Box sx={{ width: "100%" }}>
+      <AppBar
+        component="nav"
+        sx={{ display: "block", position: "static", width: "100%" }}
+      >
+        <Toolbar
+          sx={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "space-between",
+          }}
+        >
+          <Box
+            display={"flex"}
+            sx={{ alignItems: "center", justifyContent: "center" }}
+          >
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: "none" } }}
+            >
+              <MenuIcon color={"error"} />
+            </IconButton>
+
+            <Typography component="a" sx={{ lineHeight: "1" }}>
+              <Hexagon
+                sx={{ width: "32px", height: "32px" }}
+                color={"primary"}
+              />
+            </Typography>
+
+            <Box sx={{ display: { xs: "none", sm: "block" } }}>
+              {navItems.map((item) => (
+                <Button key={item} sx={{ color: "#fff" }}>
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Box>
+
+          <Button variant="contained" onClick={handleSignInClicked}>
+            Sign In
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
   );
 }
