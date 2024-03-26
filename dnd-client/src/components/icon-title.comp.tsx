6a4788d19adae3eb8edd49c70e7572e08134705e
Ilya <i.shyshparonak@gmail.com>
2024-03-26 16:17:26 +0300

    feat: create generic component to view icon titles

---
+++ dnd-client/src/components/icon-title.comp.tsx
@@ -0,0 +1,32 @@
+import { Typography, Avatar, styled, Container, Box } from "@mui/material";
+import React, { FC } from "react";
+
+type IconTitleProps = {
+  title: string;
+  Icon: React.ElementType;
+};
+
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  background: theme.palette.primary.main,
+}));
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+}));
+
+const IconTitle: FC<IconTitleProps> = ({ title, Icon }) => {
+  return (
+    <StyledBox>
+      <StyledAvatar>
+        <Icon sx={{ width: "100%", height: "100%" }} />
+      </StyledAvatar>
+      <Typography component="h1" variant="h5">
+        {title}
+      </Typography>
+    </StyledBox>
+  );
+};
+
+export default IconTitle;
