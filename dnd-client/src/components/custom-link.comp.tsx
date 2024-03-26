a7a9e48cff138e391e242445d5f17f66d603b036
Ilya <i.shyshparonak@gmail.com>
2024-03-26 16:19:20 +0300

    feat: create generic link component

---
+++ dnd-client/src/components/custom-link.comp.tsx
@@ -0,0 +1,31 @@
+import React, { FC } from "react";
+import { Box, Link, Typography, styled } from "@mui/material";
+
+interface CustomLinkProps {
+  text?: string;
+  clickableText?: string;
+  onClick: React.MouseEventHandler;
+}
+
+const StyledLink = styled(Link)({
+  cursor: "pointer",
+});
+
+const StyledBox = styled(Box)({
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  gap: "5px",
+  width: "100%",
+});
+
+const CustomLink: FC<CustomLinkProps> = ({ text, clickableText, onClick }) => {
+  return (
+    <StyledBox>
+      <Typography>{text}</Typography>
+      <StyledLink onClick={onClick}>{clickableText}</StyledLink>
+    </StyledBox>
+  );
+};
+
+export default CustomLink;
