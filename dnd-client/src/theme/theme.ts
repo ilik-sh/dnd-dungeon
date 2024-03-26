ad26add272884396166b785b4cd5d5dd4e790c24
Ilya <i.shyshparonak@gmail.com>
2024-03-26 16:11:39 +0300

    feat: update mui theme styles

--- dnd-client/src/theme/theme.ts
+++ dnd-client/src/theme/theme.ts
@@ -16,12 +16,49 @@ export const theme = createTheme({
     background: {
       paper: grey[900],
     },
+    divider: blueGrey[100],
   },
   components: {
     MuiOutlinedInput: {
       styleOverrides: {
         notchedOutline: {
           borderColor: grey[600],
+        },
+      },
+    },
+    MuiInputBase: {
+      styleOverrides: {
+        root: {
+          "&:has(> input:-webkit-autofill)": {
+            backgroundColor: grey[800],
+          },
+        },
+        input: {
+          "&:-webkit-autofill": {
+            transition: "background-color 600000s 0s, color 600000s 0s",
+          },
+        },
+      },
+    },
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          border: `2px solid ${grey[800]}`,
+          borderRadius: "10px",
+        },
+      },
+    },
+    MuiAppBar: {
+      styleOverrides: {
+        root: {
+          backgroundColor: grey[900],
+        },
+      },
+    },
+    MuiPaper: {
+      styleOverrides: {
+        root: {
+          color: blueGrey[100],
         },
       },
     },
