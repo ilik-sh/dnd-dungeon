3324bda534bb9243793da0f26095a91114acd117
Ilya <i.shyshparonak@gmail.com>
2024-03-26 16:15:00 +0300

    feat: implement sign up and sign in essential components

---
+++ dnd-client/src/app/auth/components/forms/sign-in-form.comp.tsx
@@ -0,0 +1,54 @@
+import { Button, styled } from "@mui/material";
+import { SignInForm as SignInFormFields } from "app/auth/validation-schemas/sign-in-form.schema";
+import { DungeonDoor } from "assets/icons/dungeon-door.icon";
+import { CenteredBox } from "components/centered-box.comp";
+import IconTitle from "components/icon-title.comp";
+import PasswordField from "components/password-field.comp";
+import TextField from "components/text-field.comp";
+import React from "react";
+import { Control, FieldErrors } from "react-hook-form";
+
+type SignInFormProps = {
+  onSubmit: React.FormEventHandler;
+  control: Control<SignInFormFields, any>;
+  validationErrors: FieldErrors<SignInFormFields>;
+};
+
+const StyledButton = styled(Button)(({ theme }) => ({}));
+
+const StyledForm = styled("form")({
+  display: "flex",
+  flexDirection: "column",
+  width: "100%",
+  gap: "1rem",
+});
+
+export default function SignInForm({
+  onSubmit,
+  control,
+  validationErrors,
+}: SignInFormProps) {
+  return (
+    <CenteredBox>
+      <StyledForm noValidate onSubmit={onSubmit}>
+        <TextField
+          name="Email"
+          control={control}
+          error={!!validationErrors.email}
+          helperText={validationErrors.email?.message}
+          autoComplete="email"
+        />
+        <PasswordField
+          name="Password"
+          control={control}
+          error={!!validationErrors.password}
+          helperText={validationErrors.password?.message}
+          autoComplete="current-password"
+        />
+        <StyledButton type="submit" variant="contained" fullWidth>
+          Sign In
+        </StyledButton>
+      </StyledForm>
+    </CenteredBox>
+  );
+}
