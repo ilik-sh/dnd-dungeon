542938c617a58a7e3a9ea8248ae88de65319924f
Ilya <i.shyshparonak@gmail.com>
2024-04-06 15:46:59 +0300

    feat: changed api error handling function

--- dnd-client/src/api/handle-api-error.ts
+++ dnd-client/src/api/handle-api-error.ts
@@ -2,8 +2,10 @@ import { ApiError } from "app/auth/types/api.error";
 import axios from "axios";
 
 export const handleApiError = (error: any) => {
-  if (axios.isAxiosError<ApiError>(error)) {
-    return error.response?.data as ApiError;
+  if (axios.isAxiosError(error)) {
+    const message = error.response?.statusText;
+    const statusCode = error.response?.status;
+    return { message, statusCode } as ApiError;
   }
   return {
     message: "Unknown erorr",
