d42f487050402117279b10d683c2f6ded9cefee5
Ilya Shyshparonak <i.shyshparonak@gmail.com>
2024-06-16 15:22:30 +0300

    feat: change order of room directions

--- dnd-client/src/enums/directions.enum.ts
+++ dnd-client/src/enums/directions.enum.ts
@@ -2,9 +2,9 @@ export enum Directions {
   TOP_LEFT = 'TOP_LEFT',
   TOP = 'TOP',
   TOP_RIGHT = 'BOTTOM',
-  BOTTOM_RIGHT = 'BOTTOM_RIGHT',
-  BOTTOM = 'BOTTOM_LEFT',
   BOTTOM_LEFT = 'TOP_RIGHT',
+  BOTTOM = 'BOTTOM_LEFT',
+  BOTTOM_RIGHT = 'BOTTOM_RIGHT',
 }
 
 export type Direction = keyof typeof Directions;
