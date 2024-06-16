27726f7ef3ac5aaa0e4e8d7511149151a978e84f
Ilya Shyshparonak <i.shyshparonak@gmail.com>
2024-06-16 12:42:44 +0300

    feat: temporarily remove @million/lint

--- dnd-client/vite.config.ts
+++ dnd-client/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-const plugins = [react(), tsconfigPaths(), MillionLint.vite()];
+const plugins = [react(), tsconfigPaths()];
 
 export default defineConfig({
   plugins: plugins,
