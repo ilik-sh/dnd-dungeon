2a229bb47b252f42c4da28c5e5ea9cbb5706337a
Ilya Shyshparonak <i.shyshparonak@gmail.com>
2024-06-17 14:11:26 +0300

    feat: update map showcase section

--- dnd-client/src/app/landing/components/map-showcase.section.tsx
+++ dnd-client/src/app/landing/components/map-showcase.section.tsx
@@ -1,30 +1,34 @@
-import { Box, Typography, keyframes, styled } from '@mui/material';
-import Scrollable from 'components/hoc/scrollable.comp';
-import Map from 'components/map/map.comp';
 import React from 'react';
 
-type Props = {};
+import { Box, styled, Typography } from '@mui/material';
+
+import Scrollable from 'components/hoc/scrollable.comp';
+import Map from 'components/map/map.comp';
+
+const StyledSection = styled('section')(({ theme }) => ({
+  paddingBottom: '40px',
+  background: theme.palette.landing.main,
+}));
 
 const StyledWrapper = styled(Box)({
-  paddingTop: '20px',
+  padding: '0 20px',
   height: 'auto',
-  width: '100%',
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
-  background: '#101113',
+
   boxShadow: '0px -10px 40px 30px #101113',
 });
 
 const StyledScroll = styled(Scrollable)({
+  minWidth: '90%',
   gap: '10px',
 });
 
-export default function MapShowcaseSection({}: Props) {
+export default function MapShowcaseSection() {
   return (
-    <section>
+    <StyledSection>
       <StyledWrapper>
-        <Typography variant="h2" sx={{ padding: '10px', color: 'white', fontWeight: '600' }}>
+        <Typography variant="h2" sx={{ color: 'white', fontWeight: '600' }}>
           Community created maps
         </Typography>
         <StyledScroll>
@@ -32,8 +36,10 @@ export default function MapShowcaseSection({}: Props) {
           <Map></Map>
           <Map></Map>
           <Map></Map>
+          <Map></Map>
+          <Map></Map>
         </StyledScroll>
       </StyledWrapper>
-    </section>
+    </StyledSection>
   );
 }
