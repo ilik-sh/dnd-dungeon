8230042f7c4ede1c412b3b8310f9f4771d85c7f4
4Siris <leonid_voynov@mail.ru>
2024-06-25 19:12:34 +0300

    feat: add database relations between user and mapview

--- dnd-server/src/main/java/org/example/server/domain/Models/account/User.java
+++ dnd-server/src/main/java/org/example/server/domain/Models/account/User.java
@@ -1,9 +1,11 @@
 package org.example.server.domain.Models.account;
 
 
+import com.fasterxml.jackson.annotation.JsonView;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.server.domain.Models.MapView;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +27,7 @@ public class User implements UserDetails {
     private String username;
 
     @Column(name = "password", nullable = false)
+    @JsonView(Views.inner.class)
     private String password;
 
     @Column(name = "email", unique = true, nullable = false)
@@ -32,10 +35,15 @@ public class User implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false)
+    @JsonView(Views.inner.class)
     private Role role;
 
     @Column(name = "profileAvatar")
     private String img;
+
+    @Column(name = "createdMaps")
+    @OneToMany()
+    private Set<MapView> createdMaps;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
