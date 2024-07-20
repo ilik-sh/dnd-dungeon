a6b08a8334ed522b9dbc51fadb6c7d47bce4fe2f
4Siris <leonid_voynov@mail.ru>
2024-07-20 14:54:59 +0300

    feat: add default constants

--- dnd-server/src/main/java/org/example/server/AllConstants.java
+++ dnd-server/src/main/java/org/example/server/AllConstants.java
@@ -3,7 +3,8 @@ package org.example.server;
 public enum AllConstants {;
     public enum StringConstants{
         DEFAULT_USERPROFILE_IMG("https://firebasestorage.googleapis.com/v0/b/dndhub-fb81c.appspot.com/o/defaults%2FuserProfileThumbnail.png?alt=media&token=a0b75353-052b-47e0-b5af-2c375f643eee"),
-        DEFAULT_MAP_IMG("https://firebasestorage.googleapis.com/v0/b/dndhub-fb81c.appspot.com/o/defaults%2FmapThumbnail.png?alt=media&token=0ca8fb5d-1468-4e3c-b0fa-dd004a28ef49");
+        DEFAULT_MAP_IMG("https://firebasestorage.googleapis.com/v0/b/dndhub-fb81c.appspot.com/o/defaults%2FmapThumbnail.png?alt=media&token=0ca8fb5d-1468-4e3c-b0fa-dd004a28ef49"),
+        DEFAULT_MAP_NAME("Dungeon");
 
         private final String value;
         StringConstants(String s){
