da442de5a89d9be782e2538f9218fed2faac5064
4Siris <leonid_voynov@mail.ru>
2024-07-20 16:15:08 +0300

    feat: combine Map nd MapView classes and their logic

--- dnd-server/src/main/java/org/example/server/domain/Models/Map.java
+++ dnd-server/src/main/java/org/example/server/domain/Models/Map.java
@@ -1,25 +1,53 @@
 package org.example.server.domain.Models;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.example.server.AllConstants;
+import org.example.server.Serializers.CellSerializer;
+import org.example.server.domain.Models.account.User;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 @Entity
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 public class Map {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
-    private String mapProfileId;
+    //View
+    private String thumbnailUrl;
+    private String name;
+    private long duplicateCount;
+    private long likeCount;
+    @Basic
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @ManyToOne
+    private User creator;
+    @JdbcTypeCode(SqlTypes.JSON)
+    private ArrayList<Tag> tags;
+
+    //Map
+    @JsonSerialize(using = CellSerializer.class)
     @JdbcTypeCode(SqlTypes.JSON)
     private Cell[][] mapLayout;
     @JdbcTypeCode(SqlTypes.JSON)
     private HashMap<String, Room> mapInfo;
+
+    public Map() {
+        mapInfo = new HashMap<>();
+        thumbnailUrl = AllConstants.StringConstants.DEFAULT_MAP_IMG.getValue();
+        name = AllConstants.StringConstants.DEFAULT_MAP_NAME.getValue();
+        duplicateCount = 0;
+        likeCount = 0;
+        createdAt = new Date(System.currentTimeMillis());
+        tags = new ArrayList<>();
+    }
 }
